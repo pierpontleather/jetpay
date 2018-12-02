772b66c8a9866ef3b057f665364be3b04de89b8f
Parker Pierpont <parker@pierpontleather.com>
2018-12-01 22:01:54 -0600

    working footer

--- footer.php
+++ footer.php
@@ -1,3 +1,16 @@
+	<footer class="footer">
+	<div class="footer-cubes">
+		<?php footerCubes(array(
+		'size' => '250'
+	)); ?>
+	</div>
+	<div class="footer-menu">
+		<h5>Footer Title</h5>
+		<h5>Footer Title</h5>
+		<h5>Footer Title</h5>
+		<h5>Footer Title</h5>
+	</div>
+</footer>
 		<?php wp_footer(); ?>
 	</body>
 </html>
--- index.php
+++ index.php
@@ -3,8 +3,15 @@
 <div>
 <h1><i class="fal fa-stroopwafel"></i> hello world!</h1>
 <?php Logo(array(
-  'color' => 'darkDisabled'
+  'color' => 'darkDisabled',
+  'size' => '100'
 )); ?>
+
+<h1>Hello</h1>
+<h2>This is a h2 tag</h2>
+<h3><a>this is an h3</a></h3>
+<?php singleCube(); ?>
+<?php multiCube(); ?>
 </div>
 
 <?php get_footer(); ?>
